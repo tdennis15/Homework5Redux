3a25b9c69943d3fbfa3f32d6b1f18e52132c3fc8
TJ Dennis <tdennis15@mail.wou.edu>
2017-11-20 11:34:59 -0800

    added db_UP.sql

---
+++ Homework5/Homework5/App Data/db_UP.sql
@@ -0,0 +1,26 @@
+﻿--DMV table for users
+
+CREATE TABLE dbo.Users
+(
+	ID			INT IDENTITY (1,1) NOT NULL,
+	
+	FirstName	NVARCHAR(64) NOT NULL,
+	MiddleName	NVARCHAR(64),
+	LastName	NVARCHAR(128) NOT NULL,
+	DOB			DateTime NOT NULL,
+
+	NAddress	NVARCHAR(128) NOT NULL,
+	NCity		NVARCHAR(64) NOT NULL,
+	NState		NVARCHAR(64) NOT NULL,
+	NZipCode	Int NOT NULL,
+	NCounty		NVARCHAR(64) NOT NULL,
+
+	CurrDate	DateTime NOT NULL,
+
+	CONSTRAINT [PK_dbo.Users] PRIMARY KEY CLUSTERED (ID ASC)
+);
+
+INSERT INTO dbo.Users (FirstName, MiddleName, LastName, DOB, NAddress, NCity, NState, NZipCode, NCounty, CurrDate) Values
+	('Adam','J','Jones','01/01/2001','123 fucks street','Salaam','Oregon','97301','Marion','11/01/2017');
+
+GO
