5b22d1eee487815be9da143d622912f1d8a1cf8f
TJ Dennis <tdennis15@mail.wou.edu>
2017-11-26 10:36:31 -0800

    New branch for views, fininshed controller for model

--- Homework5/Homework5/Controllers/HomeController.cs
+++ Homework5/Homework5/Controllers/HomeController.cs
@@ -1,37 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Data.Entity;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework5.DAL;
 using Homework5.Models;
 
 namespace Homework5.Controllers
 {
+
+    /// <summary>
+    /// Controller for the web form "DMV address update"
+    /// </summary>
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        //Context for each user that would be added to the database
+        private UserContext db = new UserContext();
+
+        //First action is to load the index page when this program is run
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        //Get: find User Details from input id
+        public ActionResult Details(int? id)
         {
-            ViewData["Message"] = "Your application description page.";
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
 
+        }
+
+        //Get : returns Users create page so they can enter input
+        public ActionResult Create()
+        {
             return View();
         }
 
-        public IActionResult Contact()
+        //Post : Users create page submit so that the database gets updated after validation
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(
+            [Bind(Include = "ID,FirstName,MiddleName,LastName,DOB,NAddress,NCity,NState,NZipCode,NCounty,CurrDate")] User user)
         {
-            ViewData["Message"] = "Your contact page.";
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("DataList");
+            }
 
-            return View();
+            return View(user);
         }
 
-        public IActionResult Error()
+        //Get: User edit see if the user the person is trying to edit is in the database.
+        public ActionResult Edit(int? id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST : Users Edit if the data has been validated and the user is found
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FirstName,MiddleName,LastName,DOB,NAddress,NCity,NState,NZipCode,NCounty,CurrDate")]User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("DataList");
+            }
+            return View(user);
+        }
+
+
+        // GET : Users Delete removing records if they exist
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users Delete removing from the database
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("DataList");
+        }
+
+        public ActionResult DataList()
+        {
+            return View(db.Users.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
